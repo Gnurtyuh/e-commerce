4526effb00f98ff66a3a8ecc99552cb0ad5b2e5f
Trung Huy <nkocsoc2004@gmail.com>
2026-01-22 00:05:18 +0700

    feat(user): add login api and registry api

---
+++ user-service/src/main/java/com/project/userservice/entity/Users.java
@@ -0,0 +1,39 @@
+package com.project.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@RequiredArgsConstructor(access = AccessLevel.PUBLIC)
+@Builder
+@Table(name ="users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Users {
+    @Id
+    @Column(name = "user_id", nullable = false, updatable = false)
+    @GeneratedValue
+    UUID userId;
+    @Column(name = "password")
+    String password;
+    @Column(name = "email")
+    String email;
+    @Column(name = "phone")
+    String phone;
+    @Column(name = "full_name")
+    String fullName;
+    @Column(name = "role")
+    String role;
+    @Column(name = "status")
+    String status;
+    @Column(name = "created_at")
+    Timestamp createdAt;
+    @Column(name = "updated_at")
+    Timestamp updatedAt;
+}
