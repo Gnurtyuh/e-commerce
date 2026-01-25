f649f12a5811aaa41c3a19d12df54b718ea9d26e
Trung Huy <nkocsoc2004@gmail.com>
2026-01-25 21:50:38 +0700

    feat(address): complete crud address

--- user-service/src/main/java/com/project/userservice/repository/AddressRepository.java
+++ user-service/src/main/java/com/project/userservice/repository/AddressRepository.java
@@ -2,11 +2,15 @@ package com.project.userservice.repository;
 
 import com.project.userservice.entity.Addresses;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Addresses, UUID> {
-
+    @Query("SELECT a from Addresses a where a.userId = ?1 and a.isDefault= true")
+    Addresses findByAddressIdAndDefault(UUID userId);
+    List<Addresses> findByUserId(UUID userId);
 }
---
+++ user-service/src/main/java/com/project/userservice/service/AddressService.java
@@ -0,0 +1,70 @@
+package com.project.userservice.service;
+
+import com.project.userservice.dto.request.AddressRequest;
+import com.project.userservice.dto.response.AddressResponse;
+import com.project.userservice.entity.Addresses;
+import com.project.userservice.mapper.AddressMapper;
+import com.project.userservice.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+
+public class AddressService {
+    @Autowired
+    private AddressRepository addressRepository;
+    @Autowired
+    private UserService userService;
+
+    public void createAddress(AddressRequest address) {
+        if (userService.findByUserId(address.getUserId())==null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        Addresses addresses = AddressMapper.toAddresses(address);
+        address.setAddressId(null);
+        Addresses address2 = addressRepository.findByAddressIdAndDefault(address.getUserId());
+        if (address.getIsDefault() && address2 != null) {
+            address2.setIsDefault(false);
+        }
+        addressRepository.save(addresses);
+    }
+    public void updateAddress(AddressRequest address) {
+        if (findByAddressId(address.getAddressId())==null) {
+            throw new RuntimeException("Address not found");
+        }
+        if (userService.findByUserId(address.getUserId())==null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        if(address.getIsDefault()){
+            Addresses addresses = addressRepository.findByAddressIdAndDefault(address.getUserId());
+            addresses.setIsDefault(false);
+            addressRepository.save(addresses);
+        }
+        Addresses addresses = AddressMapper.toAddresses(address);
+        addressRepository.save(addresses);
+    }
+    public void deleteAddress(UUID  addressId) {
+        if (addressId == null) {
+            throw new RuntimeException("address not found");
+        }
+        addressRepository.deleteById(addressId);
+    }
+    public List<AddressResponse> getByUserId(UUID userId) {
+        return addressRepository.findByUserId(userId)
+                .stream()
+                .map(AddressMapper::toAddressResponse)
+                .collect(Collectors.toList());
+    }
+
+    public Addresses findByAddressId(UUID addressId) {
+        return addressRepository.findById(addressId).orElse(null);
+    }
+    public AddressResponse getAddress(UUID addressId) {
+        return AddressMapper.toAddressResponse(findByAddressId(addressId));
+    }
+}
